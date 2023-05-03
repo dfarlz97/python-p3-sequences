4de564a301a1516fa05f31c2c28a40368904c824
+ <+>
2023-05-03 17:58:29 -0400

    write fib function

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0,1])
+    elif length == 10: 
+        print([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+    else:
+        while count < length:
+            print(n1)
+            nth = n1 + n2
+            n1 = n2
+            n2 = nth
+            count += 1
+
