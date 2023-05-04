ba459debe679e6d84fd1d4aef0753e883e814aac
+ <+>
2023-05-04 09:47:46 -0400

    adjust output

--- lib/sequences.py
+++ lib/sequences.py
@@ -3,17 +3,10 @@
 def print_fibonacci(length):
     if length == 0:
         print([])
-    elif length == 1:
-        print([0])
-    elif length == 2:
-        print([0,1])
-    elif length == 10: 
-        print([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
     else:
         while count < length:
-            print(n1)
+            print([n1], end="")
             nth = n1 + n2
             n1 = n2
             n2 = nth
             count += 1
-
